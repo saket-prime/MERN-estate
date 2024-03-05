7d96ff5fc00ce34f70bcf9772c8de89b905b9195
saket-prime <SAKETKUMAR0504@GMAIL.COM>
2024-03-05 09:56:10 +0530

    update header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,31 @@
 import { FaSearch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
   
   const { currentUser } = useSelector(state => state.user)
+  const [searchTerm, setSearchTerm] = useState('');
+  const navigate = useNavigate();
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('searchTerm',searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  }
+  
+  useEffect(() => {
+    
+    const urlParams = new URLSearchParams(location.search)
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }    
+  },[location.search]);
+  
   
   return (
     <header className='bg-sky-300 shadow-md'>
@@ -15,9 +36,16 @@ export default function Header() {
                 <span className='text-sky-950'>Estate</span>
             </h1>
             </Link>
-            <form className='bg-sky-200 p-3 rounded-md flex items-center'>
-                <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none  w-24 sm:w-64'></input>
-                <FaSearch className='text-sky-950 '/>
+            <form onSubmit={handleSubmit} className='bg-sky-200 p-3 rounded-md flex items-center'>
+                <input 
+                type='text' 
+                placeholder='Search...' 
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)} 
+                className='bg-transparent focus:outline-none  w-24 sm:w-64'/>
+                <button>
+                  <FaSearch className='text-sky-950 '/>
+                </button>
             </form>
             <ul className='flex gap-4'>
                   <Link to='/'><li className='font-medium hidden text-xl sm:inline  text-sky-700 hover:underline'>Home</li></Link>
