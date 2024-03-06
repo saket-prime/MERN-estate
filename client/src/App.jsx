e765588a5dd9d17a10db869f192fe12dfb4ee3ec
saket-prime <SAKETKUMAR0504@GMAIL.COM>
2024-03-06 13:35:43 +0530

    fix listing issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import EditListing from './pages/EditListing';
-import Listing from './pages/listing';
+import Listing from './pages/Listing';
 import Search from './pages/Search';
 
 export default function App() {
